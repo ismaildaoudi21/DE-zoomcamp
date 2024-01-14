59409c4f9f89586e27a3faf546ca593224b98930
ismaildaoudi21 <daoudi.ismail21@gmail.com>
2024-01-14 15:39:07 +0100

    param flow gh commit

--- week_2/homework/github_deploy.py
+++ week_2/homework/github_deploy.py
@@ -1,14 +1,15 @@
 from prefect.deployments import Deployment
 from parameterized_flow import etl_parent_flow
-from prefect.infrastructure.docker import DockerContainer
+from prefect.filesystems import GitHub
 
-docker_block = DockerContainer.load("zoomcamp")
+github_block = GitHub.load("zoomcamp")
 
-docker_dep = Deployment.build_from_flow(
+
+github_dep = Deployment.build_from_flow(
     flow=etl_parent_flow,
-    name='docker-flow',
-    infrastructure=docker_block
+    name='github_flow'
+    fs = RemoteFileSystem(basepath="s3://my-bucket/folder/")
 )
 
 if __name__ == '__main__':
-    docker_dep.apply()
+    github_dep.apply()
